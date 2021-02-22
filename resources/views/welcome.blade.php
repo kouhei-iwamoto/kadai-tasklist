d81fa8b68aedf124bec42bdadc18717368a2575b
kohei.iwamoto <pgja74527452@gmail.com>
2021-02-22 12:55:05 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>今日はどんなタスクをしようか。</h1>
-             {{-- ユーザ登録ページへのリンク --}}
+            <h1>タスクを管理しよう</h1>
+            {{-- ユーザ登録ページへのリンク --}}
             {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
